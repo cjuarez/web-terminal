67df39cdf930331d7da5d80a9e1f32df5e57c846
Celso Juarez <celsojuarez@gmail.com>
2014-09-26 01:02:58 -0500

    Added mockjax for fake webservice
    Added mockjax for fake webservice. Updated styles and added a caret.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,27 @@
-$('.terminal').on('click', function () {
-    $('.js-last-line').focus();
-});
+$(function () {
+    $.mockjax({
+      url: '/rest/execute',
+      responseTime: 150,
+      type: 'post',
+      responseText: 'I got it. Now give me the next command.'
+    });
+    var status = 'celsojuarez at MBA in ~/Github/web-terminal';
+    $('<div class="status">').text(status).appendTo($('.js-previous-commands'));
+    $('.terminal').on('click', function () {
+        $('.js-last-line').focus();
+    });
 
-$('.js-last-line').on('keypress', function (e) {
-    var key = e.keyCode;
-    if (key === 13) {
-        $('<div>').text($.trim($('.js-last-line').text())).appendTo($('.js-previous-commands'));
-        $('.js-last-line').empty();
-    }
+    $('.js-last-line').on('keypress', function (e) {
+        var key = e.keyCode;
+        if (key === 13) {
+            var command = $('.js-last-line').text();
+            $('<div>').text(command).appendTo($('.js-previous-commands'));
+            $.post('/rest/execute', command, function (response) {
+                $('<div>').text(response).appendTo($('.js-previous-commands'));
+                $('<div class="status">').text(status).appendTo($('.js-previous-commands'));
+            });
+            $('.js-last-line').empty();
+            return false;
+        }
+    });
 });
